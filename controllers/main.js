0ad41554287bfe07f3e2dbc5d547e12ac0ac4714
Ch0c0latka <149702725+Ch0c0latka@users.noreply.github.com>
2024-05-28 18:30:12 +0300

    Comment to commit

--- controllers/main.js
+++ controllers/main.js
@@ -3,7 +3,7 @@ const {getData, endpoints} = require('../appModules/api')
 const {config, makeRatingFile} = require('../appModules/rating')
 
 async function mainRouteController(res, publicUrl, extname) {
-  const data = await getData(endpoints.games);
+  const data = await getData(endpoints.game);
     await makeRatingFile(config.PATH_TO_RATING_FILE, data);
     res.statusCode = 200;
     staticFile(res, publicUrl, extname);
